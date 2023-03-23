1c8ac3e274a7dde547c24f885fbde0f2989c4f53
B-NARENDRA-REDDY <128729215+B-NARENDRA-REDDY@users.noreply.github.com>
2023-03-23 22:57:59 +0530

    Create leap year.c

---
+++ leap year.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ int year;
+ printf("Enter the year:");
+ scanf("%d",&year);
+ if(year%4==0)
+  printf("%d is leap year");
+ else
+  printf("%d is not aa leap year");
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Statu
