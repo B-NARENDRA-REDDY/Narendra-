3f4d13437d72fe916cea34a40659a102f919c3cf
B-NARENDRA-REDDY <128729215+B-NARENDRA-REDDY@users.noreply.github.com>
2023-03-23 23:02:19 +0530

    Create Positive or negative.c

---
+++ Positive or negative.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+ float n;
+ scanf("%f",&n);
+ if(n>0)
+  printf("n is positive");
+ else if(n<0)
+  printf("n is negative");
+ else
+  printf("n is zero");
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
